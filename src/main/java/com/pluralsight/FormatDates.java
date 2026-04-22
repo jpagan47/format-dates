5c9c528570924b4e98a172c16e6bf5be36c85196
josepagan <josepaganrnj@outlook.com>
2026-04-22 09:52:12 -0400

    Added Local Date code working on adding more functionality

--- src/main/java/com/pluralsight/FormatDates.java
+++ src/main/java/com/pluralsight/FormatDates.java
@@ -1,4 +1,13 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class FormatDates {
+    public static void main(String[] args) {
+        LocalDate todaysdate = LocalDate.now();
+        System.out.println(todaysdate);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern()
+    }
 }
