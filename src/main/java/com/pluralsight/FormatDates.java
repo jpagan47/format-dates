f39692b9599ad452a243bbf6bf10b4b89d54f208
josepagan <josepaganrnj@outlook.com>
2026-04-22 10:48:05 -0400

    Added the needed Formatters and End of code !

--- src/main/java/com/pluralsight/FormatDates.java
+++ src/main/java/com/pluralsight/FormatDates.java
@@ -1,13 +1,28 @@
 package com.pluralsight;
 
+import java.sql.SQLOutput;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class FormatDates {
     public static void main(String[] args) {
-        LocalDate todaysdate = LocalDate.now();
-        System.out.println(todaysdate);
+        LocalDateTime today = LocalDateTime.now();
+        LocalDate todaysDate = LocalDate.now();
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern()
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("E, MM dd, yyyy HH:mm");
+        String todayFormatted = today.format(formatter);
+
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        String dateformattted = todaysDate.format(dateFormatter);
+
+
+        System.out.println(dateformattted);
+        System.out.println(todaysDate);
+        System.out.println(todayFormatted + today.getDayOfWeek());
+
+
     }
 }
